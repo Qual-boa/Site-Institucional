29942a4ee48472570ac26e8f5c9855b0a786e2d9
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2024-04-27 20:29:48 -0300

    Começando tela dono deslogado

---
+++ site/src/pages/dono/Dono.jsx
@@ -0,0 +1,12 @@
+import "./Dono.module.css";
+import bannerEspuma from "../../assets/fundo-espuma-dono-deslogado.svg";
+import qualABoa from "../../assets/qualaboa-dono-deslogado.svg";
+
+function Dono() {
+    return(
+        <div className="cilindros">
+        </div>
+    );
+}
+
+export default Dono;
--- site/src/routes.js
+++ site/src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Estabelecimento from "./pages/estabelecimento/Estabelecimento";
+import Dono from "./pages/dono/Dono";
 
 
 function Rotas() {
@@ -9,6 +10,7 @@ function Rotas() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/estabelecimento" element={<Estabelecimento />} />
+                    <Route path="/cadastrar-estabelecimento" element={<Dono />} />
                 </Routes>
             </BrowserRouter>
         </>
