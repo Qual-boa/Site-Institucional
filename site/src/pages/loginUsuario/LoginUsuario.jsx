045111e482c902a757d4900bcef9368d17b7ca27
PedroPradoCho <pedro.araujo@sptech.school>
2024-04-27 19:55:26 -0300

    navbar

--- site/src/pages/loginUsuario/LoginUsuario.jsx
+++ site/src/pages/loginUsuario/LoginUsuario.jsx
@@ -1,15 +1,17 @@
 import styles from "./LoginUsuario.module.css";
 import "../../global.css";
+import NavBar from "../../components/navbar/NavBar";
+import logo from "../../assets/logo.svg";
 import imgPrincipal from "../../assets/ImgFundo.svg";
 import { Link } from 'react-router-dom';
 
 function LoginUsuario(){
-    return(
-        <div className={styles["background-image"]}>
-           <div className="containerLoginUsuario">
-                
-            </div>
-        </div>
+    return (
+        // Fragmento React para agrupar os elementos retornados
+        <>
+            <NavBar logoInicio = {logo}/>
+            
+        </>
     );
 }
 export default LoginUsuario;
