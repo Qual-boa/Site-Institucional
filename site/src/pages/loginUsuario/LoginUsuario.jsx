b9878c67543cce40f1b9364ee0ecc23701bf2829
Vinicius Costa <vinicius.costa@sptech.school>
2024-04-27 17:56:57 -0300

    Subindo alterações no css

--- site/src/pages/loginUsuario/LoginUsuario.jsx
+++ site/src/pages/loginUsuario/LoginUsuario.jsx
@@ -1,13 +1,12 @@
-import "./LoginUsuario.module.css";
+import styles from "./LoginUsuario.module.css";
 import "../../global.css";
 import imgPrincipal from "../../assets/ImgFundo.svg";
 import { Link } from 'react-router-dom';
 
 function LoginUsuario(){
     return(
-        <div className="background-image">
-            <img src={imgPrincipal} alt="Imagem de fundo" />
-            <div className="containerLoginUsuario">
+        <div className={styles["background-image"]}>
+           <div className="containerLoginUsuario">
                 
             </div>
         </div>
