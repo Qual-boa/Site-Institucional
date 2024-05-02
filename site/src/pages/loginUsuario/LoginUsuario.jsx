b32a8af7227b01bd1b1f7e6796d502663d48db5c
PedroPradoCho <pedro.araujo@sptech.school>
2024-05-02 16:50:58 -0300

    ajustes em navbar

--- site/src/pages/loginUsuario/LoginUsuario.jsx
+++ site/src/pages/loginUsuario/LoginUsuario.jsx
@@ -1,20 +1,18 @@
 import styles from "./LoginUsuario.module.css";
 import "../../global.css";
-import NavBar from "../../components/navbar/NavBar";
-import logo from "../../assets/logo.svg";
 import { CardLogin } from "../../components/cardLogin/CardLogin";
-import { Footer } from "../../components/footer/Footer";
+import NavBar from "../../components/navbar/NavBar";
+import logo from "../../assets/QualABoa.svg";
 
 function LoginUsuario(){
     return(
         <>
-            <NavBar logoInicio = {logo}/>
-                <div className={styles["background-image"]}>
-                        <div className={styles["containerLoginUsuario"]}>
-                    <CardLogin />
-                    </div> 
-                </div>
-            <Footer />     
+        <NavBar logoInicio = {logo}/>
+            <div className={styles["background-image"]}>
+                    <div className={styles["containerLoginUsuario"]}>
+                <CardLogin />
+                </div> 
+            </div>    
         </>
     );
 }
--- site/src/routes.js
+++ site/src/routes.js
@@ -3,18 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Estabelecimento from "./pages/estabelecimento/Estabelecimento";
 import LoginUsuario from "./pages/loginUsuario/LoginUsuario"
 import LoginEmpresa from "./pages/loginEmpresa/LoginEmpresa"
-
+import Footer from "./components/footer/Footer";
 
 function Rotas() {
     return (
         <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/estabelecimento" element={<Estabelecimento />} />
-                    <Route path="/login" element={<LoginUsuario/>}/>
-                    <Route path="/loginEmpresa" element={<LoginEmpresa/>}/>
-                </Routes>
-            </BrowserRouter>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/estabelecimento" element={<Estabelecimento />} />
+                        <Route path="/login" element={<LoginUsuario/>}/>
+                        <Route path="/loginEmpresa" element={<LoginEmpresa/>}/>
+                    </Routes>
+                </BrowserRouter> 
+            <Footer />       
         </>
     )
 }
