4aa1a562486e9301c9fb41710227ee6ea1ec2a90
PedroPradoCho <pedro.araujo@sptech.school>
2024-05-02 18:50:55 -0300

    footer finalizado

--- site/src/components/footer/Footer.jsx
+++ site/src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import styles from "./Footer.module.css";
 import LogoFooter from '../../assets/footerIcon.svg'
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import LogoFooter2024 from "../../assets/footer2024.svg"
 
 function Footer() {
     return (
@@ -12,16 +14,33 @@ function Footer() {
                 <img src={LogoFooter} alt="logo" className={styles.logo} />
             </div>
             <div className={styles.menuContainer}>
-                <div className={styles.menu}><span>MENU</span>
-                    <span>
-                        
-                    </span>
+                <div className={styles.menu}>
+                    <h3 className={styles.menuTitulo}>MENU</h3>
+                    <a className={styles.options} onClick={""}>QUAL A BOA?</a>
+                    <a className={styles.options} onClick={""}>MELHORES DO MÊS</a>
+                    <a className={styles.options} onClick={""}>QUAL A SUA BOA?</a>
+                    <a className={styles.options} onClick={""}>QUEM SOMOS</a>
+                    <a className={styles.options} onClick={""}>CONTATOS</a>                    
                 </div>
-                <div className={styles.menu}><span>ENDEREÇO</span>
-
+                <div className={styles.menu}>
+                    <h3 className={styles.menuTitulo}>ENDEREÇO</h3>
+                    <span className={styles.optionsEndereco}>Rua Haddock Lobo, 595</span>
+                    <span className={styles.optionsEndereco}>Cerqueira César</span>
+                    <span className={styles.optionsEndereco}>São Paulo - SP</span>
+                    <span className={styles.optionsEndereco}>01414-00</span>
                 </div>
-                <div className={styles.menu}><span>CONTATO</span>
-                
+                <div className={styles.menu}>
+                    <h3 className={styles.menuTitulo}>CONTATOS</h3>
+                    <span className={styles.options}>@QualABoa</span>
+                    <FaFacebook className={styles.midias}/>
+                    <FaInstagram className={styles.midias}/>
+                    <FaLinkedin className={styles.midias}/>
+                </div>
+            </div>
+            <div className={"footer_copyright_container"} >
+                <div className={styles.imag}>
+                    <img src={LogoFooter2024} alt="Qual a Boa 2024" className={styles.logo}/>
+                    <span>Todos os direitos reservados</span>
                 </div>
             </div>
         </footer>)
