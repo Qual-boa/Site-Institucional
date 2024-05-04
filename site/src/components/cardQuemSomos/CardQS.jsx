613b84ceb7f156c770750a8f94a340008348e3da
CarlosHBenecke <carlos.benecke@sptech.school>
2024-05-04 15:38:16 -0300

    tela quem-somos

---
+++ site/src/components/cardQuemSomos/CardQS.jsx
@@ -0,0 +1,12 @@
+import styles from "./CardQS.module.css";
+import "../../global.css";
+
+
+function CardQS() {
+    return (
+        <div className={styles["containerQS"]}> {/* Adicione esta div */}        
+           
+        </div>
+    );
+}
+export default CardQS;
