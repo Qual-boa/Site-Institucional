9f2553e2b669a1776ea744dedeb072f5bfff302c
CarlosHBenecke <carlos.benecke@sptech.school>
2024-05-04 17:28:26 -0300

    conexão com api, verificação de senhas na tela-cadastro-usuario

--- site/src/components/cardCadastro/CardCadastro.jsx
+++ site/src/components/cardCadastro/CardCadastro.jsx
@@ -2,8 +2,42 @@ import styles from "./CardCadastro.module.css";
 import "../../global.css";
 import avatar from "../../assets/asset-cadastro-usuario/avatar.svg"; // Importe o seu arquivo de imagem SVG
 import { Link } from 'react-router-dom';
+import api from "../../api";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
 
 function CardCadastro() {
+    const navigate = useNavigate();
+    const [nome,setNome] = useState("");
+    const [email,setEmail] = useState("");
+    const [senha,setSenha] = useState("");
+    const [senhaConfirmacao] = useState("");
+    const salvarUsuario =()=>{
+
+        if (senha !== senhaConfirmacao) {
+            alert("Senhas não coincidem. Tente novamente");toast.error("As senhas não coincidem.");
+            return; 
+        }
+
+        api.post("/users/register",{
+            name: nome,
+            email,
+            password: senha,
+            userTypeEnum: "COMMON",
+            roleEnum: "USER"
+        }).then(()=>{alert("Usuário Cadastrado com sucesso!");toast.success("Novo Usuário cadastrado com sucesso!");
+            navigate("/UsuarioFinal")
+        }).catch(()=>{alert("Erro ao efetuar o cadastro, tente novamente");toast.error("Ocorreu um erro ao salvar os dados, por favor, tente novamente.")})
+    }
+
+    const setarValoresInput=(evento, setState)=>{
+        setState(evento.target.value)
+    }
+    
+    
+
     return (
         <div className={styles["containerCadastroUsuario"]}> {/* Adicione esta div */}        
             <div className={styles["card"]}>            
@@ -13,17 +47,21 @@ function CardCadastro() {
                     <form>
                         <div className={styles["form-group"]}>
                             <label htmlFor="email"><b>E-MAIL:</b></label>
-                            <input type="email" className={styles["form-control"]} id="email" aria-describedby="emailHelp" placeholder="Digite seu e-mail"/>
+                            <input type="email" className={styles["form-control"]} id="email" aria-describedby="emailHelp" placeholder="Digite seu e-mail" onChange={(e) => setarValoresInput(e, setEmail)}/>
+                        </div>
+                        <div className={styles["form-group"]}>
+                            <label htmlFor="nome"><b>NOME COMPLETO:</b></label>
+                            <input type="text" className={styles["form-control"]} id="nome" aria-describedby="nomeHelp" placeholder="Digite seu nome completo" onChange={(e) => setarValoresInput(e, setNome)}/>
                         </div>
                         <div className={styles["form-group"]}>
                             <label htmlFor="senha"><b>SENHA:</b></label>
-                            <input type="password" className={styles["form-control"]} id="senha" placeholder="Digite sua senha" />
+                            <input type="password" className={styles["form-control"]} id="senha" placeholder="Digite sua senha" onChange={(e) => setarValoresInput(e, setSenha)}/>
                         </div>
                         <div className={styles["form-group"]}>
                             <label htmlFor="senhaConfirmacao"><b>CONFIRME A SENHA:</b></label>
                             <input type="password" className={styles["form-control"]} id="senhaConfirmacao" placeholder="Digite sua senha novamente" />
                         </div>
-                        <button type="submit" className={styles["botao-entrar-usuario"]}>CADASTRAR</button>
+                        <button type="submit" className={styles["botao-entrar-usuario"]} onClick={salvarUsuario}>CADASTRAR</button>
                         <div className={styles["loginExistente"]}>
                             Já possui Login?<Link to="/login" className={styles.entrar}>Entrar</Link>
                         </div>
