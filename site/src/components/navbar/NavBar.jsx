8028ead8cd55449600619886447ef6cc7970f158
CarlosHBenecke <carlos.benecke@sptech.school>
2024-05-08 21:10:40 -0300

    link para quem somos na NavBar e links externos para GitHub

--- site/src/components/navbar/NavBar.jsx
+++ site/src/components/navbar/NavBar.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './NavBar.module.css';
 import perfil from '../../assets/perfilUser.svg';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = ({ logoInicio }) => {
+    const navigate = useNavigate();
     return (
         <nav className={styles["navbar"]}>
             <img src={logoInicio} className={styles["logo-inicio"]} alt="Logo Início" />
             <span><b>MELHORES DO MÊS</b></span>
             <span><b>QUAL A SUA BOA?</b></span>
             <span><b>CONTATOS</b></span>
-            <span><b>QUEM SOMOS</b></span>
+            <span onClick={() => navigate("/quem-somos")}><b>QUEM SOMOS</b></span>
             <input type="text" placeholder="Escreva aqui"/>
             <img src={perfil} className={styles["perfil-user"]} alt="Usuário" />
         </nav>
--- site/src/pages/quemSomos/NavBarQS/navBarQS.jsx
+++ site/src/pages/quemSomos/NavBarQS/navBarQS.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './navBarQS.module.css';
 import perfil from '../../../assets/perfilBranco.svg';
-
+import { useNavigate } from 'react-router-dom';
 
 const NavBarQS = ({ logoQS }) => {
+    const navigate = useNavigate();
     return (
         <nav className={styles["navbarQS"]}>
             <img src={logoQS} className={styles["logoQS"]} alt="Logo Início" />
             <span><b>MELHORES DO MÊS</b></span>
             <span><b>VANTAGENS</b></span>
             <span><b>CONTATOS</b></span>
-            <span><b>QUEM SOMOS</b></span>
+            <span onClick={() => navigate("/quem-somos")}><b>QUEM SOMOS</b></span>
             <input type="text" placeholder="Escreva aqui"/>
             <img src={perfil} className={styles["perfil-userQS"]} alt="Usuário" />
         </nav>
