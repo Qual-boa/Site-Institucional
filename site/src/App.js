6e656c56f943dafa29ee15510c674d149b9eb099
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2024-06-13 10:46:02 -0300

    updating app.js imports

--- site/src/App.js
+++ site/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Rotas from "./routes";
-import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 function App() {
