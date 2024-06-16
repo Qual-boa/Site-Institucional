379512855cbef3dd8d4f218991dd35ad324ae520
PedroPradoCho <pedro.araujo@sptech.school>
2024-06-16 03:20:38 -0300

    ainda to mexendo

--- site/src/pages/listagemEstabelecimento/Listagem.jsx
+++ site/src/pages/listagemEstabelecimento/Listagem.jsx
@@ -9,26 +9,26 @@ import Footer from "../../components/footer/Footer";
 function Listagem() {
     const [tamanhoDivExterno, setTamanhoDivExterno] = useState(0);
     const divRef = useRef(null);
-  
+
     useEffect(() => {
-      if (divRef.current) {
-        setTamanhoDivExterno(divRef.current.offsetHeight);
-      }
+        if (divRef.current) {
+            setTamanhoDivExterno(divRef.current.offsetHeight);
+        }
     }, []);
-  
+
     return (
-      <>
-        <Navbar logoInicio={logo} />
-        <section className={styles["sessao"]} id="inicio">
-          <div className={styles["background-image"]} style={{ height: `calc(100vh + ${tamanhoDivExterno}px)` }}>
-            <div className={styles["containerBusca"]} ref={divRef}>
-              <ResultadoBusca />
-            </div>
-          </div>
-        </section>
-        <Footer />
-      </>
+        <>
+            <Navbar logoInicio={logo} />
+            <section className={styles["sessao"]} id="inicio">
+                <div className={styles["background-image"]} style={{ height: `calc(100vh - ${tamanhoDivExterno}px)` }}>
+                    <div className={styles["containerBusca"]} ref={divRef}>
+                        <ResultadoBusca />
+                    </div>
+                </div>
+            </section>
+            <Footer className={styles.footer} />
+        </>
     );
-  }
-  export default Listagem;
-  
+}
+
+export default Listagem;
