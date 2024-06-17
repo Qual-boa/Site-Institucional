356155fe6ca26a269db9554a0fef98bf2b7339be
PedroPradoCho <pedro.araujo@sptech.school>
2024-06-17 00:29:16 -0300

    preciso fixa o conteinar da pesquisa de listagem só, nn to conseguindo

--- site/src/components/footer/Footer.jsx
+++ site/src/components/footer/Footer.jsx
@@ -1,58 +1,27 @@
-import styles from "./Footer.module.css";
-import LogoFooter from '../../assets/footerIcon.svg'
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
-import LogoFooter2024 from "../../assets/footer2024.svg"
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import styles from './Footer.module.css';
+import LogoFooter from '../../assets/footerIcon.svg';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
-
-    const navigate = useNavigate();
-
-    const quemSomosSection = (sectionId) => {
-        // Navega para a página inicial (ou para a página onde está a seção desejada)
-        navigate('/quem-somos');
-
-        // Espera um pequeno intervalo de tempo antes de rolar para a seção
-        setTimeout(() => {
-            var secao = document.getElementById(sectionId);
-            if (secao) {
-                secao.scrollIntoView({ behavior: 'smooth' });
-            }
-        }, 100); // ajuste o tempo conforme necessário
-    };
-
-    const scrollToSection = (sectionId) => {
-        // Navega para a página inicial (ou para a página onde está a seção desejada)
-        navigate('/home');
-        // Espera um pequeno intervalo de tempo antes de rolar para a seção
-        setTimeout(() => {
-            var secao = document.getElementById(sectionId);
-            if (secao) {
-                secao.scrollIntoView({ behavior: 'smooth' });
-            }
-        }, 100); // ajuste o tempo conforme necessário
-    };
-
-
     return (
         <footer className={styles.footer}>
             <div className={styles.logoContainer}>
-                <img src={LogoFooter} alt="logo" className={styles.logo} style={{ margin: '13px' }}/>
                 <img src={LogoFooter} alt="logo" className={styles.logo} />
                 <img src={LogoFooter} alt="logo" className={styles.logo} />
                 <img src={LogoFooter} alt="logo" className={styles.logo} />
                 <img src={LogoFooter} alt="logo" className={styles.logo} />
                 <img src={LogoFooter} alt="logo" className={styles.logo} />
-
+                <img src={LogoFooter} alt="logo" className={styles.logo} />
             </div>
             <div className={styles.menuContainer}>
-            <div className={styles.menu}>
+                <div className={styles.menu}>
                     <h3 className={styles.menuTitulo}>MENU</h3>
-                    <a className={styles.options} href="#outra" smooth={true} onClick={() => scrollToSection('cidades')}>CIDADES MAIS PROCURADAS</a>
-                    <a className={styles.options} href="#outra" smooth={true} onClick={() => scrollToSection('bares')}>BARES MAIS VISTOS</a>
-                    <a className={styles.options} href="#outra" smooth={true} onClick={() => scrollToSection('sugestoes')}>SUGESTÔES DO MÊS</a>
-                    <a className={styles.options} href="#outra" smooth={true} onClick={() => scrollToSection('boa')}>QUAL A SUA BOA?</a>                    
-                    <a className={styles.options} href="#outra" smooth={true} onClick={() => quemSomosSection('quem-somos')}>QUEM SOMOS</a>
+                    <a className={styles.options} href="#outra">CIDADES MAIS PROCURADAS</a>
+                    <a className={styles.options} href="#outra">BARES MAIS VISTOS</a>
+                    <a className={styles.options} href="#outra">SUGESTÕES DO MÊS</a>
+                    <a className={styles.options} href="#outra">QUAL A SUA BOA?</a>
+                    <a className={styles.options} href="#outra">QUEM SOMOS</a>
                 </div>
                 <div className={styles.menu}>
                     <h3 className={styles.menuTitulo}>ENDEREÇO</h3>
@@ -64,17 +33,16 @@ function Footer() {
                 <div className={styles.menu}>
                     <h3 className={styles.menuTitulo}>CONTATOS</h3>
                     <span className={styles.options}>@QualABoa</span>
-                    <FaFacebook className={styles.midias}/>
-                    <FaInstagram className={styles.midias}/>
-                    <FaLinkedin className={styles.midias}/>
+                    <FaFacebook className={styles.midias} />
+                    <FaInstagram className={styles.midias} />
+                    <FaLinkedin className={styles.midias} />
                 </div>
             </div>
-            <div className={"footer_copyright_container"} >
-                <div className={styles.copyright}>
-                    <img src={LogoFooter2024} alt="Qual a Boa 2024" className={styles.logo}/>
-                    <span>Todos os direitos reservados</span>
-                </div>
+            <div className={styles.copyright}>
+                <span>Todos os direitos reservados</span>
             </div>
-        </footer>)
+        </footer>
+    );
 }
-export default Footer
+
+export default Footer;
