5b11769a65075b12792fdb56c87f7b57f69df4ec
Samuel Lucena <samuellucenadeabreu@hotmail.com>
2024-10-16 22:40:25 -0300

    Dockerfile react e correcao

--- site/src/api.js
+++ site/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api-qualaboa.azurewebsites.net" //-http://localhost:8080
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export default api;
