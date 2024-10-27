cea9b62c5ff20efb66bfff0cb7bc28c3b1882f37
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2024-10-27 16:25:28 -0300

    Dockerfile

--- site/src/api-blob.js
+++ site/src/api-blob.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBlob = axios.create({
-    baseURL: "https://ec2-3-87-212-101.compute-1.amazonaws.com/api/ms-blob/blob"
+    baseURL: "https://ec2-54-234-187-50.compute-1.amazonaws.com/api/ms-blob/blob"
 })
 
 export default apiBlob;
